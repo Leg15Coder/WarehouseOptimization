1c7ce6d98b4cd59ff53c79050059c869437eb179
Leg15Coder <ryazhskikh.dima.76@mail.ru>
2025-04-24 17:56:32 +0300

    Добавлена блокирующая защита от асинхронности

--- src/algorithm/app.py
+++ src/algorithm/app.py
@@ -14,17 +14,55 @@ class SizeType(Enum):
 
 
 class Algorithm:
+    class __FlagContainer:
+        def __init__(self):
+            self.flag = False
+            self.request = SelectionRequest()
+
+        def __ior__(self, other: SelectionRequest):
+            self.requests |= other
+            return self
+
+        def __isub__(self, other: SelectionRequest):
+            self.request -= other
+            return self
+
+        def __invert__(self):
+            self.flag = not self.flag
+            return self
+
+        def __pos__(self):
+            self.flag = True
+            return self
+
+        def __neg__(self):
+            self.flag = False
+            return self
+
+        def __bool__(self):
+            return self.flag
+
+        def __iter__(self):
+            return ((product, self.request[product]) for product in self.request)
+
+    class ProductWrapper:
+        def __init__(self, product: Product, nearest_deadline: datetime):
+            self.product = product
+            self.nearest_deadline = nearest_deadline
+
     warehouse: Warehouse
     clusters_controller: Clusterizer
     size_type: SizeType
 
     requests_queue: PriorityQueue[SelectionRequest]
-    deadline_checker = None
-    products_stack: dict[Product, int]
+    requests_in_wait: dict[Product, int]
+    requests_in_process: dict[Product, int]
 
-    deadline_flag: bool = False
-    full_stack_flag: bool = False
-    one_product_left_flag: bool = False
+    deadline_flag: __FlagContainer = __FlagContainer()
+    full_stack_flag: __FlagContainer = __FlagContainer()
+    one_product_left_flag: __FlagContainer = __FlagContainer()
+
+    locker: threading.Lock = threading.Lock()
 
     def __init__(self):
         self.warehouse = Warehouse()
@@ -32,7 +70,8 @@ class Algorithm:
         self.size_type = self.clusters_controller.analyze()
 
         self.requests_queue = PriorityQueue()
-        self.products_stack = dict()
+        self.requests_in_wait = dict()
+        self.requests_in_process = dict()
 
         threading.Thread(target=self.check_flags, daemon=True).start()
 
@@ -42,36 +81,75 @@ class Algorithm:
         threading.Thread(target=self._schedule_deadline_check, daemon=True).start()
 
         while True:
-            if self.deadline_flag or self.full_stack_flag or self.one_product_left_flag:
-                pass
+            self.run_algorithm()
+            asyncio.sleep(1)
 
     def _watch_flags_loop(self):
         while True:
-            for product, count in self.products_stack.items():
-                if count >= product.max_per_hand:
-                    self.full_stack_flag = True
+            local_flag = False
+            for wrapper, count in self.requests_in_wait.items():
+                if count >= wrapper.product.max_per_hand:
+                    self.full_stack_flag |= SelectionRequest(((product, count), ))
+                    local_flag = True
+
+            if local_flag:
+                self.full_stack_flag = +self.full_stack_flag
             asyncio.sleep(1)
 
     def _watch_one_product_left(self):
         while True:
+            continue
+            local_flag = False
             for request in self.requests_queue:
-                remaining = self._remaining_products(request)
-                if len(remaining) == 1:
-                    self.one_product_left_flag = True
+                if abs(request) == 1:  # Остался последний товар
+                    self.one_product_left_flag |= request
+                    local_flag = True
+
+            if local_flag:
+                self.one_product_left_flag = +self.one_product_left_flag
             asyncio.sleep(1)
 
     def _schedule_deadline_check(self):
-        oldest_request: SelectionRequest = None
-        deadline_time: time = None
-
         while True:
-            top_request: SelectionRequest = self.requests_queue.queue[0]
-            if top_request is not oldest_request:
-                oldest_request = top_request
-                deadline_time = deadline_time - timedelta(seconds=5)
-            if datetime.now() - datetime <= 0:
-                self.deadline_flag = True
+            local_flag = False
+
+            for wrapper, count in self.requests_in_wait.items():
+                if wrapper.nearest_deadline + timedelta(seconds=5) >= datetime.now():
+                    self.deadline_flag |= SelectionRequest(((wrapper.product, count), ))
+                    local_flag = True
+
+            if local_flag:
+                self.deadline_flag = +self.deadline_flag
             asyncio.sleep(1)
+
+    async def run_algorithm(self):
+        clusters = None
+        if self.deadline_flag:
+            with self.locker:
+                await self.add_to_process(self.deadline_flag.request)
+                clusters = await self.choose_clusters(self.deadline_flag.request)
+
+        elif self.full_stack_flag:
+            with self.locker:
+                await self.add_to_process(self.full_stack_flag.request)
+                clusters = await self.choose_clusters(self.full_stack_flag.request)
+
+        elif self.one_product_left_flag:
+            with self.locker:
+                await self.add_to_process(self.one_product_left_flag.request)
+                clusters = await self.choose_clusters(self.one_product_left_flag.request)
+
+        if clusters:
+            cells = self.choose_cells(clusters)
+            path = self.build_way(cells)
+            return path
+
+    async def add_to_process(self, request: SelectionRequest) -> None:
+        with self.locker:
+            for product in request:
+                count = request[product]
+                self.requests_in_wait[ProductWrapper(product, None)] -= count
+                self.requests_in_process[ProductWrapper(product, None)] += count
 
     async def solve(self, request: SelectionRequest) -> Optional[str]:
         pass
